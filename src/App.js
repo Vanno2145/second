56c7f0b412437d39d04e911c047313315b9537df
John Doe <johndoe@example.com>
2025-06-19 19:13:20 +0300

    ch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import { Medals } from './Medals';
+import { Description } from './Description';
+import { Players } from './Players';
+
+const currentSquad = [
+  { firstName: "Алексей", lastName: "Громов", birthYear: 1995 },
+  { firstName: "Илья", lastName: "Морозов", birthYear: 1997 },
+  { firstName: "Сергей", lastName: "Швец", birthYear: 1996 },
+  { firstName: "Максим", lastName: "Белов", birthYear: 1998 },
+  { firstName: "Даниил", lastName: "Плахотнюк", birthYear: 1999 },
+  { firstName: "Владимир", lastName: "Елисеев", birthYear: 1994 },
+  { firstName: "Егор", lastName: "Карасёв", birthYear: 2000 },
+  { firstName: "Никита", lastName: "Журавлёв", birthYear: 1996 },
+  { firstName: "Антон", lastName: "Лавров", birthYear: 1993 },
+  { firstName: "Артур", lastName: "Синицын", birthYear: 1997 },
+  { firstName: "Роман", lastName: "Серебров", birthYear: 1995 }
+];
+
+
+let btn = document.getElementById('first')
+
+btn.addEventListener("click", function(){
+  starr.
+})
+
+let starr = [st1, st2, st3]
+
+
+let st1 = {
+  backgroungColor: 'black'
+}
+
+let st2 = {
+  backgroungColor: 'yellow'
+}
+
+let st3 = {
+  backgroungColor: 'red'
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id = 'main' style={st}>
+      <div id = 'Themes'>
+        <button id = 'first'>1</button>
+      </div>
+      <Description></Description>
+      <Medals></Medals>
+      <Players arr = {currentSquad}></Players>
     </div>
   );
 }
